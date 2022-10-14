e0edc2e671bb229c3111d43278892fd2120ccc81
b001418671 <b00141867@mytudublin.ie>
2022-10-14 19:16:59 +0100

    Update README.md

---
+++ hyperlink/Assets/scripts/openlink.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openlink : MonoBehaviour
+{
+ public void OpenChannel(){
+      Application.OpenURL("https://github.com/b001418671/creat-with-code-lab-3.git");
+ }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
